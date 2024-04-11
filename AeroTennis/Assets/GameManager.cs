f7108de776893c3b59c8c8696047cb847ad2f5d0
xOcarin <119756949+xOcarin@users.noreply.github.com>
2024-04-11 14:29:43 -0400

    more menu stuff

--- AeroTennis/Assets/GameManager.cs
+++ AeroTennis/Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject objectToDisable;
+    public VideoPlayer videoPlayer;
     public GameObject objectToEnable;
+    public GameObject objectToDisable;
     public AudioSource audioSource;
     public AudioClip audioClip;
     public float delayBeforeAction = 32f;
@@ -17,12 +19,23 @@ public class GameManager : MonoBehaviour
         Invoke("PerformAction", delayBeforeAction);
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PerformAction();
+        }
+    }
+
     void PerformAction()
     {
         if (!hasPerformedAction)
         {
-            if (objectToDisable != null)
+            if (videoPlayer != null)
+            {
+                videoPlayer.Stop();
                 objectToDisable.SetActive(false);
+            }
 
             if (objectToEnable != null)
                 objectToEnable.SetActive(true);
